e15ea67283abe704b8ccebde651952cfe24b973b
Milton Olaf Paredes <miltonolaf@outlook.com>
2018-10-02 21:47:25 -0500

    test

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -x
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-    cd _site
-    git init
 
-    git remote add deploy "deploy@104.248.70.250:/var/www/miltonolaf.com"
-    git config user.name "Travis CI"
-    git config user.email "olafmilton+travisCI@gmail.com"
+cd _site
+git init
 
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-else
-    echo "Not deploying, since this branch isn't master."
-fi
+git remote add deploy "ssh://deploy@104.248.70.250:/var/www/miltonolaf.com"
+git config user.name "Travis CI"
+git config user.email "olafmilton+travisCI@gmail.com"
+
+git add .
+git commit -m "Deploy"
+git push --force deploy master
